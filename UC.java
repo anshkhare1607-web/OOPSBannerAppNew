a0242520758edec18be74e1fcfe085222103b4ef
anshkhare1607-web <anshkhare1607@gmail.com>
2026-02-18 14:31:13 +0530

    Banner Printing using + operator

---
+++ UC.java
@@ -0,0 +1,18 @@
+/*
+*UC2: Feature/UC2-Print OOPS Banner
+*@author Developer
+*version 2.0
+*/
+
+public class UC2{
+	public static void main(String[] args){
+		System.out.println("    ***         ***      **********   *******");
+		System.out.println("  **   **     **   **    **      **   **     ");
+		System.out.println(" **     **   **     **   **      **    *****");
+		System.out.println(" **     **   **     **   **********      ****");
+		System.out.println(" **     **   **     **   **                **");
+		System.out.println("  **   **     **   **    **           **  ***");
+		System.out.println("    ***         ***      **           *******");
+	}
+
+}
