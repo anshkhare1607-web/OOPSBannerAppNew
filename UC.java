7e3e3ac367cea52010dc7c63c71e5a9adf5edc43
anshkhare1607-web <anshkhare1607@gmail.com>
2026-02-18 15:07:11 +0530

    Coded Banner App using String.join() function

---
+++ UC.java
@@ -0,0 +1,17 @@
+/**
+*UC3: Feature/UC3-OOPS Banner Appllication
+*@author Developer
+*version 3.0
+*/
+
+public class UC{
+	public static void main(String[] args){
+		System.out.println(String.join(" ","   ***   ","   ***   ","*******","******"));
+		System.out.println(String.join(" "," **   ** "," **   ** ","**    *","**    "));
+		System.out.println(String.join(" ","**     **","**     **","**    *","***** "));
+		System.out.println(String.join(" ","**     **","**     **","*******"," *****"));
+		System.out.println(String.join(" ","**     **","**     **","**     ","    **"));
+		System.out.println(String.join(" "," **   ** "," **   **" ,"**     ","**   *"));
+		System.out.println(String.join(" ","   ***   ","   ***   ","**     ","******"));
+	}
+}
