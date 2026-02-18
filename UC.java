c4877c86304b848b2bd99c4481c46dc55ffbdb11
anshkhare1607-web <anshkhare1607@gmail.com>
2026-02-18 14:25:31 +0530

    Coded UC1 to Print OOPS to Console

---
+++ UC.java
@@ -0,0 +1,12 @@
+/**
+*UC1: Feature/UC1-PrintOOPS
+*@author Developer
+*version 1.0
+*/
+
+class Message{
+   public static void main(String[] args){
+      System.out.println("Jai OOPS");
+	  
+   }
+}
