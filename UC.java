565f7c2d8346c707c2d8d3c57bcee635052e9ea5
anshkhare1607-web <anshkhare1607@gmail.com>
2026-02-19 11:58:18 +0530

    Coded Banner App using InnerClass and Static Classes

--- UC.java
+++ UC.java
@@ -1,54 +1,60 @@
-
 /**
-*UC6: Feature/UC6-OOPS Banner Printing using methods 
+*UC7: Feature/UC7-OOPS Banner Printing using Inner Classes and Static Classes 
 *@author Developer
-*version 6.0
+*version 7.0
 */
 
-public class UC{
-	
-	public static String[] oPattern(){ //TO print the O letter
-		return new String[] {
-			"   ***   ",
-			" **   ** ",
-			"**     **",
-			"**     **",
-			"**     **",
-			" **   ** ",
-			"   ***   "
-		};
-	}
-	
-	public static String[] sPattern(){ //TO print the S letter
-		return new String[] {
-			"******",
-			"**    ",
-			"***** ",
-			" *****",
-			"    **",
-			"**   *",
-			"******"
-		};
-	}
-	
-	public static String[] pPattern(){ // To print the P letter
-		return new String[] {
-			"*******",
-			"**    *",
-			"**    *",
-			"*******",
-			"**     ",
-			"**     ",
-			"**     "
-		};
-	}
+
+class UC{
 	public static void main(String[] args){
-	
-		String[] getO = oPattern();
-		String[] getP = pPattern();
-		String[] getS = sPattern();
+		BannerApp.CharacterPatternMap charmap = new BannerApp.CharacterPatternMap();
+		String[] getO = charmap.oPattern();
+		String[] getP = charmap.pPattern();
+		String[] getS  =charmap.sPattern();
 		for(int i=0;i<getO.length;i++){
 			System.out.println(getO[i]+" "+getO[i]+" "+getP[i]+" "+getS[i]);
 		}
 	}
 }
+class BannerApp{
+	static class CharacterPatternMap{
+		public static String[] oPattern(){ //TO print the O letter
+			return new String[] {
+				"   ***   ",
+				" **   ** ",
+				"**     **",
+				"**     **",
+				"**     **",
+				" **   ** ",
+				"   ***   "
+			};
+		}
+		
+		public static String[] sPattern(){ //TO print the S letter
+			return new String[] {
+				"******",
+				"**    ",
+				"***** ",
+				" *****",
+				"    **",
+				"**   *",
+				"******"
+			};
+		}
+		
+		public static String[] pPattern(){ // To print the P letter
+			return new String[] {
+				"*******",
+				"**    *",
+				"**    *",
+				"*******",
+				"**     ",
+				"**     ",
+				"**     "
+			};
+		}
+		
+	}
+}
+
+
