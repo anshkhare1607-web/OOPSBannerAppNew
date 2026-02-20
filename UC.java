4e6550e6c65f6d8e00a60ac486194951f0987394
anshkhare1607-web <anshkhare1607@gmail.com>
2026-02-20 11:02:21 +0530

    Printing Banner using HashMap

--- UC.java
+++ UC.java
@@ -1,60 +1,68 @@
 /**
-*UC7: Feature/UC7-OOPS Banner Printing using Inner Classes and Static Classes 
+*UC8: Feature/UC8-OOPS Banner Printing using HashMaps 
 *@author Developer
-*version 7.0
+*version 8.0
 */
 
 
-class UC{
-	public static void main(String[] args){
-		BannerApp.CharacterPatternMap charmap = new BannerApp.CharacterPatternMap();
-		String[] getO = charmap.oPattern();
-		String[] getP = charmap.pPattern();
-		String[] getS  =charmap.sPattern();
-		for(int i=0;i<getO.length;i++){
-			System.out.println(getO[i]+" "+getO[i]+" "+getP[i]+" "+getS[i]);
-		}
-	}
-}
-class BannerApp{
-	static class CharacterPatternMap{
-		public static String[] oPattern(){ //TO print the O letter
-			return new String[] {
-				"   ***   ",
-				" **   ** ",
-				"**     **",
-				"**     **",
-				"**     **",
-				" **   ** ",
-				"   ***   "
-			};
-		}
-		
-		public static String[] sPattern(){ //TO print the S letter
-			return new String[] {
-				"******",
-				"**    ",
-				"***** ",
-				" *****",
-				"    **",
-				"**   *",
-				"******"
-			};
-		}
-		
-		public static String[] pPattern(){ // To print the P letter
-			return new String[] {
-				"*******",
-				"**    *",
-				"**    *",
-				"*******",
-				"**     ",
-				"**     ",
-				"**     "
-			};
-		}
-		
-	}
-}
+import java.util.HashMap;
 
+public class UC{
+	public static HashMap<Character,String[]> createCharacterMap(){ //Method for creating a HashMap with all patterns of letters O,P,S.
+		HashMap<Character, String[]> map = new HashMap<>();
+		
+		map.put('O', new String[]{ //Storing 'O' pattern as a value for key 'O'.
+			"   ***   ",
+		    " **   ** ",
+			"**     **",
+			"**     **",
+			"**     **",
+			" **   ** ",
+			"   ***   "
+		});
+		
+		map.put('P', new String[] { //Storing 'P' pattern as a value for key 'P'.
+			"*******",
+			"**    *",
+			"**    *",
+			"*******",
+			"**     ",
+			"**     ",
+			"**     "
+		});
+		
+		map.put('S', new String[] { //Storing 'P' pattern as a value for key 'P'.
+			"******",
+			"**   *",
+			"***** ",
+			" *****",
+			"    **",
+			"*    *",
+			"******"
+		});
+		
+		return map;
+		
+	}
+	
+	//Method for printing OOPS Banner. Storing the patterns line by line in  a stringbuilder and then printing it
+	public static void displayBanner(String message, HashMap<Character, String[]> map){
+		for(int i=0;i<7;i++){
+			StringBuilder sb = new StringBuilder();
+			for(char c:message.toCharArray()){
+				//System.out.print(map.get(c)[i] + " ");
+				String[] pattern = map.get(c);
+				sb.append(pattern[i]).append(" ");
+			}
+			System.out.println(sb);
+		}
+	}
+	
+	public static void main(String[] args){
+		HashMap<Character, String[]> map = createCharacterMap();
+		String message = "OOPS";
+		displayBanner(message, map);
+	}
+		
+}
 
